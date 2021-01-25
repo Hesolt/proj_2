00668675ddeb991d50640ac5925a861e65a59834
Yuriy Maryshev <ymaryshev@alanis-software.com>
2021-01-25 17:06:35 +0700

    Добавьте файлы проекта.

---
+++ proj_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace proj_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50;
+            int b = 70;
+            Console.WriteLine($"{b - a}");
+        }
+    }
+}
